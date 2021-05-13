db14c40ceaaf2b09ee2463e4ac13e75391ce6e3f
Rushabh Singh <rushabh.singh@carwale.com>
2021-05-13 10:39:09 +0530

    Title : Creating lazy image view for efficient rendering

---
+++ BookMyShow/LazyImageVIew.swift
@@ -0,0 +1,34 @@
+//
+//  LazyImageVIew.swift
+//  BookMyShow
+//
+//  Created by Rushabh Singh on 13/05/21.
+//
+
+import Foundation
+import UIKit
+
+class LazyImageView: UIImageView
+{
+    private let imageCache = NSCache<AnyObject, UIImage>()
+    
+    func loadImage(fromURL imageUrl: URL, placeHolderImage: String){
+        if let cachedImage = self.imageCache.object(forKey: imageUrl as AnyObject){
+            DispatchQueue.main.async {
+                self.image = cachedImage
+            }
+        }else{
+            self.image = UIImage(named: placeHolderImage)
+            DispatchQueue.global().async { [weak self] in
+                if let imageData = try? Data(contentsOf: imageUrl){
+                    if let image = UIImage(data: imageData){
+                        DispatchQueue.main.async{
+                            self!.imageCache.setObject(image, forKey: imageUrl as AnyObject)
+                            self?.image = image
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
